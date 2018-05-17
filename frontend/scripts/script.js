3d25db4cf99d33df4fbf783ee58b4a8358f06673
Maxim Stepanov <developer.stepanov@gmail.com>
2018-05-17 10:42:50 +0200

    Changes in scroll and button - does not jump

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -20,8 +20,6 @@ function init() {
 	var game_name = $('.mission-container.selected').attr('id');
 
 	getTemplateDays('./backend/data/' + game_name + '.json');
-
-
 };
 
 function checkReservationButton() {
@@ -174,6 +172,8 @@ function renderTemplate(data, callback) {
 	//arrow
 	$('.day.scroll-arrow-right').addClass('active');
 
+	$('.day-container.selected').children().css({"zoom": "130%"});
+
 	addEventsForTemplateDays();
 
 	//add some aditional logic after rendering template days
@@ -198,32 +198,34 @@ function addEventsForTemplateDays() {
 					$(event.target).addClass('selected');
 				}
 			}
+		}
 
-			/*
-			*	different day
-			*/
-			if (!$(this).hasClass('selected')) {
-				//remove old selection
-				$(this).siblings('.selected').children('.time-slot').removeClass('selected');
-				$(this).siblings('.selected').removeClass('selected');
+		/*
+		*	different day
+		*/
+		if (!$(this).hasClass('selected')) {
+			$('.day-container.selected').children().animate({"zoom": "100%"}, 80);
+			//remove old selection
+			$(this).siblings('.selected').children('.time-slot').removeClass('selected');
+			$(this).siblings('.selected').removeClass('selected');
 
-				//add new selection
-				$(this).addClass('selected');
-				if($(event.target).hasClass('time-slot')) {
-					$(event.target).addClass('selected');
-				}
-
-				var i = $('.day-container').index(this);
-
-				//change pixels of elements to make animation
-				var pixels = i * 200;
-	      		$('.day-container').attr('style', 'left: -' + pixels + 'px;');
-
-	 			checkArrows('day-container', i, 'day');
+			//add new selection
+			$(this).addClass('selected');
+			if($(event.target).hasClass('available')) {
+				$(event.target).addClass('selected');
 			}
 
-			checkReservationButton();
+			var i = $('.day-container').index(this);
+
+			//change pixels of elements to make animation
+			var pixels = i * 200;
+			$('.day-container.selected').children().animate({"zoom": "130%"}, 300);
+      		$('.day-container').attr('style', 'left: -' + pixels + 'px;');
+ 			checkArrows('day-container', i, 'day');
 		}
+
+		checkReservationButton();
+		//}
 	});
 
 	//event listener : onclick-scroll by click on arrows
@@ -231,7 +233,8 @@ function addEventsForTemplateDays() {
 
 		if ($(event.target).hasClass('scroll-arrow')) {
 			var selected_time_onfocus = $(this).find('.day-container.selected');
-			
+			$(selected_time_onfocus).children().animate({"zoom": "100%"}, 70);
+
 			var i = $('.day-container').index(selected_time_onfocus);
 
 			//press the left arrow
@@ -244,6 +247,7 @@ function addEventsForTemplateDays() {
 				selected_time_onfocus = $('.day-container').get(i-1);
 				$(selected_time_onfocus).addClass('selected');
 
+				$('.day-container.selected').children().animate({"zoom": "130%"}, 180);
 				$('.day-container').attr('style', 'left: -' + pixels + 'px;');
 
 				checkArrows('day-container', (i-1), 'day');
@@ -261,6 +265,7 @@ function addEventsForTemplateDays() {
 				selected_time_onfocus = $('.day-container').get(i+1);
 				$(selected_time_onfocus).addClass('selected');
 
+				$('.day-container.selected').children().animate({"zoom": "130%"}, 180);
 				$('.day-container').attr('style', 'left: -' + pixels + 'px;');
 
 				checkArrows('day-container', (i+1), 'day');
